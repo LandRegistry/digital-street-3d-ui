421a9785d32bd84a2250762ed005151a5948b8ee
Andy Mantell <andymantell@hotmail.com>
2017-01-03 14:33:49 +0000

    Setting up PostCSS and cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var path = require('path')
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss')
+var cssnano = require('cssnano');
+var autoprefixer = require('autoprefixer');
 
 var assetsPath = './flask_skeleton_ui/assets'
 var sassPath = 'src/styles/*.scss'
@@ -11,6 +14,10 @@ gulp.task('sass', function () {
   return gulp.src(path.join(assetsPath, sassPath))
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano()
+    ]))
     .pipe(sourcemaps.write('sourcemaps'))
     .pipe(gulp.dest(path.join(assetsPath, 'static/.dist/css')))
 });
