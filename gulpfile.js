a60a458169293175afd1b6211f1f2c9956e8ad51
Andy Mantell <andymantell@hotmail.com>
2017-01-03 13:59:59 +0000

    Enabling sourcemaps for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var path = require('path')
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps');
 
 var assetsPath = './flask_skeleton_ui/assets'
 var sassPath = 'src/styles/*.scss'
 
 gulp.task('sass', function () {
   return gulp.src(path.join(assetsPath, sassPath))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(path.join(assetsPath, 'static/.dist/css')));
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('sourcemaps'))
+    .pipe(gulp.dest(path.join(assetsPath, 'static/.dist/css')))
 });
 
 gulp.task('watch', function () {
-  gulp.watch(path.join(assetsPath, sassPath), ['sass']);
-});
+  gulp.watch(path.join(assetsPath, sassPath), ['sass'])
+})
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass'])
