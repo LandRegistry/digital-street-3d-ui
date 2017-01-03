366fdd569060dc4cbaed4d41debff9944c4fd201
Andy Mantell <andymantell@hotmail.com>
2017-01-03 14:58:39 +0000

    Adding rollup to the gulp build pipeline to bundle JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var path = require('path')
 
 var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
 var sass = require('gulp-sass')
-var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss')
-var cssnano = require('cssnano');
-var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano')
+var autoprefixer = require('autoprefixer')
+var rollup = require('rollup-stream')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
 
 var assetsPath = './flask_skeleton_ui/assets'
 var sassPath = 'src/styles/*.scss'
@@ -18,12 +21,26 @@ gulp.task('sass', function () {
       autoprefixer(),
       cssnano()
     ]))
-    .pipe(sourcemaps.write('sourcemaps'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(path.join(assetsPath, 'static/.dist/css')))
+});
+
+gulp.task('js', function() {
+  return rollup({
+      entry: path.join(assetsPath, 'src/javascript/main.js'),
+      sourceMap: true
+    })
+    .pipe(source('main.js', path.join(assetsPath, 'src/javascript')))         // point to the entry file.
+    .pipe(buffer())                           // buffer the output. most gulp plugins, including gulp-sourcemaps, don't support streams.
+    .pipe(sourcemaps.init({loadMaps: true}))  // tell gulp-sourcemaps to load the inline sourcemap produced by rollup-stream.
+    // .pipe(rename('index.js'))                // if you want to output with a different name from the input file, use gulp-rename here.
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.join(assetsPath, 'static/.dist/javascript')))
 });
 
 gulp.task('watch', function () {
   gulp.watch(path.join(assetsPath, sassPath), ['sass'])
+  gulp.watch(path.join(assetsPath, 'src/javascript/**/*.js'), ['js'])
 })
 
-gulp.task('default', ['sass'])
+gulp.task('default', ['sass', 'js'])
