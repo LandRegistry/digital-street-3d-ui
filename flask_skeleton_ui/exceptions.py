88209a2a35b75592127c4a334778cd52eb48c046
Andy Mantell <andymantell@hotmail.com>
2017-05-10 10:13:24 +0100

    Rewriting exception handlers

--- flask_skeleton_ui/exceptions.py
+++ flask_skeleton_ui/exceptions.py
@@ -1,59 +1,104 @@
+from flask import current_app
 from flask import jsonify
 from flask import render_template
 from flask_skeleton_ui import utils
+from jinja2 import TemplateNotFound
 from werkzeug.exceptions import default_exceptions
-from werkzeug.exceptions import HTTPException
 
 
-GENERIC_ERROR_TITLE = 'Sorry, we are experiencing technical difficulties.'
-GENERIC_ERROR_DESCRIPTION = 'Please try again in a few moments.'
+class ApplicationError(Exception):
+    """
+    This class is to be raised when the application identifies that there's been a problem
+    and that the user should be informed.
 
-ERROR_RESPONSES = {
-    404: {
-        'title': 'Page not found',
-        'description': 'If you entered a web address please check it was correct.'
-    },
-    429: {
-        'title': 'Too many requests',
-        'description': 'The maximum rate allowed is {description}.'
-    },
-    403: {
-        'title': 'Access denied',
-        'description': 'You do not have permission to access the requested resource.'
-    }
-}
+    This should only be used for absolute edge case exceptions.
+    As a matter of course, exceptions should be caught and dealt with higher up
+    in the flow and users should be given a decent onward journey.
+
+    Consider security issues when writing messages - what information might you
+    be revealing to potential attackers?
+
+    Example:
+        raise ApplicationError('Friendly message here', 'E102', 400)
+    """
+    def __init__(self, message, code=None, http_code=500):
+        Exception.__init__(self)
+        self.message = message
+        self.http_code = http_code
+        self.code = code
 
 
-def error_handler(error):
-    if isinstance(error, HTTPException):
-        code = error.code
-        error_title = GENERIC_ERROR_TITLE
-        error_description = GENERIC_ERROR_DESCRIPTION
+def unhandled_exception(e):
+    current_app.logger.exception('Unhandled Exception: %s', repr(e))
 
-        error_response = ERROR_RESPONSES.get(code, False)
-        if(error_response):
-            error_title = error_response.get('title', GENERIC_ERROR_TITLE)
-            error_description = error_response.get('description', GENERIC_ERROR_DESCRIPTION).format(**error.__dict__)
-    else:
-        code = 500
-        error_title = GENERIC_ERROR_TITLE
-        error_description = GENERIC_ERROR_DESCRIPTION
+    http_code = 500
 
     # Negotiate based on the Accept header
     if utils.request_wants_json():
-        return jsonify({'message': '{} - {}'.format(error_title, error_description)}), code
+        return jsonify({}), http_code
     else:
-        return render_template("error.html",
-                               title=error_title,
-                               code=code,
-                               description=error_description
-                               ), code
+        return render_template('errors/unhandled.html',
+                               http_code=http_code,
+                               ), http_code
+
+
+def http_exception(e):
+    current_app.logger.exception('HTTP Exception: %s', repr(e))
+
+    # Restrict error codes to a subset so that we don't inadvertently expose
+    # internal system information via error codes
+    if e.code in [500, 404, 403, 429]:
+        http_code = e.code
+    else:
+        http_code = 500
+
+    # Negotiate based on the Accept header
+    if utils.request_wants_json():
+        return jsonify({}), http_code
+    else:
+        return render_template('errors/unhandled.html',
+                               http_code=http_code,
+                               ), http_code
+
+
+def application_error(e):
+    current_app.logger.debug('Application Exception: %s', repr(e), exc_info=True)
+
+    # ApplicationError allows developers to specify an HTTP code.
+    # This will be written to the logs correctly, but we don't want to allow
+    # this code through to the user as it may expose internal workings of the system
+    # (See OWASP guidelines on error handling)
+    if e.http_code in [500, 404, 403, 429]:
+        http_code = e.http_code
+    else:
+        http_code = 500
+
+    if utils.request_wants_json():
+        return jsonify({
+                       'message': e.message,
+                       'code': e.code
+                       }), http_code
+    else:
+        try:
+            return render_template('errors/application/{}.html'.format(e.code),
+                                   description=e.message,
+                                   code=e.code,
+                                   http_code=http_code
+                                   ), http_code
+        except TemplateNotFound:
+            return render_template('errors/application.html',
+                                   description=e.message,
+                                   code=e.code,
+                                   http_code=http_code
+                                   ), http_code
 
 
 def register_exception_handlers(app):
+    app.register_error_handler(ApplicationError, application_error)
+    app.register_error_handler(Exception, unhandled_exception)
 
+    # Register all default HTTP exceptions from werkzeug
     for exception in default_exceptions:
-        app.register_error_handler(exception, error_handler)
+        app.register_error_handler(exception, http_exception)
 
-    app.register_error_handler(Exception, error_handler)
-    app.logger.info("Exception handlers registered")
+    app.logger.info('Exception handlers registered')
--- flask_skeleton_ui/views/index.py
+++ flask_skeleton_ui/views/index.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint, Response, render_template
 from flask import current_app
 import json
+from jinja2 import TemplateNotFound
 
 # This is the blueprint object that gets registered into the app in blueprints.py.
 index = Blueprint('index', __name__)
