7b50166a22b417ea52909009850978d12d0752a7
Andy Mantell <andymantell@hotmail.com>
2017-05-10 15:04:32 +0100

    Set some of the recommended security headers from OWASP guidelines

--- flask_skeleton_ui/extensions/register.py
+++ flask_skeleton_ui/extensions/register.py
@@ -1,21 +1,22 @@
 from flask_skeleton_ui.extensions.cachebust_static_assets.main import CachebustStaticAssets
 from flask_skeleton_ui.extensions.enhanced_logging.main import EnhancedLogging
 from flask_skeleton_ui.extensions.gzip_static_assets.main import GzipStaticAssets
+from flask_skeleton_ui.extensions.security_headers.main import SecurityHeaders
 
 
 # Create empty extension objects here
 cachebust_static_assets = CachebustStaticAssets()
 enhanced_logging = EnhancedLogging()
 gzip_static_assets = GzipStaticAssets()
+security_headers = SecurityHeaders()
 
 
 def register_extensions(app):
     """Adds any previously created extension objects into the app, and does any further setup they need."""
     enhanced_logging.init_app(app)
-
     cachebust_static_assets.init_app(app)
-
     gzip_static_assets.init_app(app)
+    security_headers.init_app(app)
 
     # All done!
     app.logger.info("Extensions registered")
---
+++ flask_skeleton_ui/extensions/security_headers/main.py
@@ -0,0 +1,21 @@
+class SecurityHeaders(object):
+    """Set some security related headers
+
+    See https://www.owasp.org/index.php/OWASP_Secure_Headers_Project#tab=Headers
+    """
+
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+
+        @app.after_request
+        def security_headers(response):
+            response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+            response.headers['Strict-Transport-Security'] = 'max-age=31536000'  # 1 year
+            response.headers['X-XSS-Protection'] = '1; mode=block'
+            response.headers['X-Content-Type-Options'] = 'nosniff'
+
+            return response
