6123a34540b985aea6f5dec43c5fe588ccb54dbe
Andy Mantell <andymantell@hotmail.com>
2017-05-18 11:21:47 +0100

    Linting fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,19 +16,12 @@ var config = {
 // hence the need to override it here
 config.moduleContext = {}
 
-try {
-  // jQuery referenced here because it'll be a common one for people to trip over
-  config.moduleContext[path.relative(process.cwd(), require.resolve('jquery'))] = 'window'
-} catch (e) {
-  // jQuery not found, no need to set up module context
-}
-
 config.assetsPath = path.join(config.applicationPath, config.assetsPath)
 
 // Register all the gulp tasks provided by the land registry module
 // If you don't want to do this, you could opt not to register some of the tasks
 // Also, if you want more tasks than this, you are free to use the gulp variable
 // and register custom tasks below
-for (task in landRegistryGulpTasks) {
+for (var task in landRegistryGulpTasks) {
   landRegistryGulpTasks[task](gulp, config)
 }
