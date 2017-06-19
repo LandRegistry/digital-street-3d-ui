1b279625dc287c81615a4f87192da631f9745368
Andy Mantell <andymantell@hotmail.com>
2017-06-19 10:17:30 +0100

    Rolling in updates to enhanced_logging from flask-skeleton-api

--- flask_skeleton_ui/custom_extensions/enhanced_logging/main.py
+++ flask_skeleton_ui/custom_extensions/enhanced_logging/main.py
@@ -1,10 +1,19 @@
 from flask import g
 from flask import request
 from flask_logconfig import LogConfig
-import logging
+from pathlib import Path
 import requests
-
 import uuid
+
+
+def before_request():
+    # Sets the transaction trace id on the global object if provided in the HTTP header from the caller.
+    # Generate a new one if it has not. We will use this in log messages.
+    g.trace_id = request.headers.get('X-Trace-ID', uuid.uuid4().hex)
+    # We also create a session-level requests object for the app to use with the header pre-set, so other APIs
+    # will receive it. These lines can be removed if the app will not make requests to other LR APIs!
+    g.requests = requests.Session()
+    g.requests.headers.update({'X-Trace-ID': g.trace_id})
 
 
 class EnhancedLogging(object):
@@ -15,21 +24,23 @@ class EnhancedLogging(object):
             self.init_app(app)
 
     def init_app(self, app):
+        # Ensure that the traceid is parsed/propagated on every request
+        app.before_request(before_request)
+
+        # Let's get the app's base package name so we can set the correct formatter, filter and logger names
+        app_module = Path(__file__).resolve().parents[2].parts[-1]
 
         logconfig = {
             'version': 1,
             'disable_existing_loggers': False,
             'formatters': {
                 'simple': {
-                    '()': 'flask_skeleton_ui.custom_extensions.enhanced_logging.formatters.JsonFormatter'
-                },
-                'audit': {
-                    '()': 'flask_skeleton_ui.custom_extensions.enhanced_logging.formatters.JsonAuditFormatter'
+                    '()': app_module + '.custom_extensions.enhanced_logging.formatters.JsonFormatter'
                 }
             },
             'filters': {
                 'contextual': {
-                    '()': 'flask_skeleton_ui.custom_extensions.enhanced_logging.filters.ContextualFilter'
+                    '()': app_module + '.custom_extensions.enhanced_logging.filters.ContextualFilter'
                 }
             },
             'handlers': {
@@ -38,22 +49,12 @@ class EnhancedLogging(object):
                     'formatter': 'simple',
                     'filters': ['contextual'],
                     'stream': 'ext://sys.stdout'
-                },
-                'audit_console': {
-                    'class': 'logging.StreamHandler',
-                    'formatter': 'audit',
-                    'filters': ['contextual'],
-                    'stream': 'ext://sys.stdout'
                 }
             },
             'loggers': {
-                'flask_skeleton_ui': {
+                app_module: {
                     'handlers': ['console'],
                     'level': app.config['FLASK_LOG_LEVEL']
-                },
-                'audit': {
-                    'handlers': ['audit_console'],
-                    'level': 'INFO'
                 }
             }
         }
@@ -63,17 +64,3 @@ class EnhancedLogging(object):
         logger = LogConfig()
 
         logger.init_app(app)
-
-        # Along with the default flask logger (app.logger) define a new one specifically for audit. To use this logger
-        # just add app.audit_logger.info("an audit point").
-        app.audit_logger = logging.getLogger("audit")
-
-        @app.before_request
-        def before_request():
-            # Sets the transaction trace id on the global object if provided in the HTTP header from the caller.
-            # Generate a new one if it has not. We will use this in log messages.
-            g.trace_id = request.headers.get('X-Trace-ID', uuid.uuid4().hex)
-            # We also create a session-level requests object for the app to use with the header pre-set, so other APIs
-            # will receive it. These lines can be removed if the app will not make requests to other LR APIs!
-            g.requests = requests.Session()
-            g.requests.headers.update({'X-Trace-ID': g.trace_id})
