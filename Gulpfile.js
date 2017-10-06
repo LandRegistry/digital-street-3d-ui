8e25e6efcc23476c9526c7774de1ba4b3c9db160
Andy Mantell <contact@andymantell.com>
2017-10-06 12:22:30 +0100

    Update gulp tasks to v2.0.0 - Switched to Webpack - Gulp config structure change

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,14 @@
 var gulp = require('gulp')
 var landRegistryGulpTasks = require('land-registry-gulp-tasks')
 
-var path = require('path')
-
 var config = {
   'applicationPath': './flask_skeleton_ui',
-  'assetsPath': 'assets',
-  'sassPath': 'src/scss/*.scss',
+  'sourcePath': './flask_skeleton_ui/assets/src',
+  'destinationPath': './flask_skeleton_ui/assets/dist',
+  'sassPath': 'scss/*.scss',
   'localhost': 'localhost:8080',
   'browsersyncPort': 3996
 }
-
-// Set up context for each module
-// Commonly used for modules that expect "this" to resolve to the window object
-// In ES6 modules, using "this" at the top level always resolves to undefined
-// hence the need to override it here
-config.moduleContext = {}
-
-config.assetsPath = path.join(config.applicationPath, config.assetsPath)
 
 // Register all the gulp tasks provided by the land registry module
 // If you don't want to do this, you could opt not to register some of the tasks
