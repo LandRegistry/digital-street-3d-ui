47700b021a880d0af66436a578c6355165d1db73
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-02-08 10:04:58 +0000

    Update land-registry-elements to latest version in requirements.txt

--- pipcompilewrapper.sh
+++ pipcompilewrapper.sh
@@ -4,5 +4,5 @@ pip-compile --output-file requirements.txt requirements.in
 pip-compile --upgrade
 
 echo "https://github.com/LandRegistry/govuk-elements-jinja-macros/archive/v1.0.7.zip" >> requirements.txt
-echo "https://github.com/LandRegistry/land-registry-elements/archive/v2.1.13.zip" >> requirements.txt
+echo "https://github.com/LandRegistry/land-registry-elements/archive/v2.1.14.zip" >> requirements.txt
 
