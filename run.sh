8d4d5504c1ffa23234bdb32a97284c0235e79ce4
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-03-06 12:00:35 +0000

    Run the frontend build pipeline in the background so that it doesn't block gunicorn from starting

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp /node_modules/flask-skeleton-ui/package-lock.json .
-npm run build
+npm run build &
 /usr/bin/gunicorn -k eventlet --pythonpath /src --access-logfile - manage:manager.app --reload
