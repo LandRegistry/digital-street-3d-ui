ff9a62588a6e25fd08b3c8bda8cd2a113b89b1c5
Andy Mantell <andy.mantell@landregistry.gov.uk>
2018-04-30 13:05:21 +0000

    Fixed typo in preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -31,7 +31,7 @@ If you are:
 ${BOLD}1)${NORMAL} Trying to install a new package:
   * Abort this command now by pressing '${BOLD}n${NORMAL}'
   * From the host, run ${BOLD}npm install --package-lock-only packageName${NORMAL}
-  * Inside the virtual machine, run ${BOLD}rebuild skeleton-webseal-templates${NORMAL}
+  * Inside the virtual machine, run ${BOLD}rebuild flask-skeleton-ui${NORMAL}
     to fetch the new modules
 
 
