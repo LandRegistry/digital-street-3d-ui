9d1d99c689077fe8df055e61a5e3320ac522d088
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-09-12 10:18:12 +0100

    Fix markdown renderer for new govuk markup

--- flask_skeleton_ui/custom_extensions/jinja_markdown_filter/gov_renderer.py
+++ flask_skeleton_ui/custom_extensions/jinja_markdown_filter/gov_renderer.py
@@ -3,6 +3,16 @@ import misaka
 
 class GovRenderer(misaka.HtmlRenderer):
 
+    def paragraph(self, content):
+        return '<p class="govuk-body">{0}</p>'.format(content)
+
+    def link(self, content, link, title):
+        return '<a class="govuk-link" href="{0}"{1}>{2}</a>'.format(
+            link,
+            ' title="{}"'.format(title) if title else '',
+            content
+        )
+
     def header(self, content, level):
         """Custom renderer to map Markdown heading levels to gov.uk heading classes
 
@@ -10,23 +20,23 @@ class GovRenderer(misaka.HtmlRenderer):
         on what methods are available to override
         """
         heading_levels = {
-            1: 'heading-xlarge',
-            2: 'heading-large',
-            3: 'heading-medium',
-            4: 'heading-small',
-            5: 'heading-small',
-            6: 'heading-small'
+            1: 'govuk-heading-xl',
+            2: 'govuk-heading-l',
+            3: 'govuk-heading-m',
+            4: 'govuk-heading-s',
+            5: 'govuk-heading-s',
+            6: 'govuk-heading-s'
         }
 
         return '<h{0} class="{1}">{2}</h{0}>'.format(level, heading_levels.get(level), content)
 
     def list(self, content, is_ordered, is_block):
         """Custom renderer to output lists with the gov.uk classes"""
-        return '<{0} class="list list-{1}">{2}</{0}>'.format(
+        return '<{0} class="govuk-list govuk-list--{1}">{2}</{0}>'.format(
             'ol' if is_ordered else 'ul',
             'number' if is_ordered else 'bullet',
             content
         )
 
-    def double_emphasis(self, content):
-        return '<strong class="bold">{}</strong>'.format(content)
+    def hrule(self):
+        return '<hr class="govuk-section-break govuk-section-break--xl govuk-section-break--visible">'
