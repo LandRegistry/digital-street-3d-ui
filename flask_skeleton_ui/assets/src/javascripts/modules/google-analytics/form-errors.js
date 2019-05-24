2296ad38b2eff37b89bb7eea1a89417384615b60
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-05-24 14:08:06 +0100

    Comment out the sending of form error to GA, and leave message regarding turning it back on again if desirable

--- flask_skeleton_ui/assets/src/javascripts/modules/google-analytics/form-errors.js
+++ flask_skeleton_ui/assets/src/javascripts/modules/google-analytics/form-errors.js
@@ -9,7 +9,20 @@ if (formErrorDataElement) {
     item.errors.forEach(error => {
       window.gtag('event', `${formName} : ${item.name}`, {
         'event_category': 'FormValidation',
-        'event_label': error
+        // Sending the actual error in the event label is commented out by default
+        // If you wish to send error messages to google analytics, you can comment out the following line
+        // However, if doing this, you must be aware that if your error messages contain any
+        // sensitive information such as email addresses, that this would be a problem from a
+        // GDPR and general privacy perspective, as well as potentially leaking other data to google.
+        //
+        // This might occur if you were to replay user input back to people in the error message, such as:
+        // "john.smith@example.com is not a valid email address"
+        //
+        // If you are confident that this is not the case, and will never be the case with your errors,
+        // then you may uncomment this line.
+        //
+        // 🐉🐉 HERE BE DRAGONS! 🐉🐉
+        // 'event_label': error
       })
     })
   })
